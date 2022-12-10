e1515c7e434593fc8af190357a4424fc5cb32418
Arsene Severin Boundaone <arsene.boundaone@gmail.com>
2022-12-10 12:00:40 -0600

    adding comment lines about the purpose of the script

--- geojson2.py
+++ geojson2.py
@@ -8,15 +8,15 @@ from pathlib import Path
 # Setting up the working directory path
 working_directory = Path(__file__).absolute().parent
 
-# This script gets the file in a geojson form then 'flattens' it so the id element 
-# in 'external_id' is at the same level as 'type', 'properties', and 'geometry'
+# This script gets the file in a geojson form then extracts the id element 
+# in 'external_id' and put it at the same level as 'type', 'properties', and 'geometry'
 with open(working_directory / 'la-zip-code-areas-2012.geojson') as f:
     json_contents = json.loads(f.read())
     features = json_contents["features"]
     for i in features:
         i["id"] = i["properties"]["external_id"]
         json_contents["features"] = features
-    # Normalized geojson for Tableau map
+    # Normalized geojson for salesforce map
     out_file = open("la_zip_code_areas_2012.json", "w")
     out_file.write(json.dumps(json_contents))
     out_file.close()
